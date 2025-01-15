2f80f4ebfe0dbe93e6992fb3f0184cad475f7fb3
silindokuhleL <silindokuhlelmapiyeye@gmail.com>
2025-01-15 12:51:32 +0200

    added register functionality

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,21 +2,24 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 use App\Services\UserService;
 use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Log;
+use App\Http\Requests\CreateUserRequest;
 
 class UserController extends Controller
 {
     /**
-     * @param Request $request
+     * @param CreateUserRequest $request
      * @param UserService $service
      * @return Response
      */
-    public function store(Request $request, UserService $service): Response
+    public function store(CreateUserRequest $request, UserService $service): Response
     {
         try {
-            return $this->json($service->createUser($request->all())->toArray());
+            return $this->json($service->createUser($request->validated())->toArray());
         } catch (\Exception $ex) {
             return $this->jsonServerError($ex->getMessage());
         }
