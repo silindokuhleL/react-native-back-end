2b9908e7f52dc4e991978d56e716fa870f738465
silindokuhleL <silindokuhlelmapiyeye@gmail.com>
2025-01-29 10:08:56 +0200

    fixed register controller git status

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -12,15 +12,14 @@ use App\Http\Requests\CreateUserRequest;
 class UserController extends Controller
 {
     /**
-     * @param CreateUserRequest $request
+     * @param Request $request
      * @param UserService $service
      * @return Response
      * */
-    public function store(CreateUserRequest $request, UserService $service): Response
+    public function store(Request $request, UserService $service): Response
     {
         try {
-
-            return $this->json($$service->createUser($request->all())->toArray());
+            return $this->json($service->createUser($request->all())->toArray());
         } catch (\Exception $ex) {
 
             return $this->jsonServerError($ex->getMessage());
