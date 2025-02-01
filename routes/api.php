ac03b05d88a9547755bb087cf645c33394a7d95a
silindokuhleL <silindokuhlelmapiyeye@gmail.com>
2025-02-02 01:06:02 +0200

    added role functionality

--- routes/api.php
+++ routes/api.php
@@ -6,7 +6,15 @@ use App\Http\Controllers\UserController;
 
 Route::middleware(['auth:sanctum'])->group(function () {
     Route::get('/user', function (Request $request) {
-        return $request->user();
+        $user = $request->user();
+        return response()->json([
+            'id' => $user->id,
+            'name' => $user->name,
+            'email' => $user->email,
+            'roles' => $user->getRoleNames(),
+            'permissions' => $user->getAllPermissions()->pluck('name'),
+            'email_verified_at' => $user->email_verified_at,
+        ]);
     });
     Route::post('/logout', [UserController::class, 'destroy']);
 });
